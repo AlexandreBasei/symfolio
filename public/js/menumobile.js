317602d6c9582d1967ce4a1810ea6d5a81e661f1
Your Name~Oweno <owen.donnars@gmail.com>
2023-04-10 23:09:01 +0200

    ADD CSS & Menumobile

--- public/js/menumobile.js
+++ public/js/menumobile.js
@@ -2,8 +2,29 @@ document.addEventListener('DOMContentLoaded', function() {
 const menu = document.getElementById('menu_burger');
 const menu_deroulant = document.getElementById('menu_deroulant');
 
+
+// Récupérez l'élément body
+var body = document.querySelector('body');
+var hey = 0;
+
 menu.addEventListener('click', () => {
-    menu.classList.toggle('ouvert');
-    menu_deroulant.classList.toggle('menu_deroulant');
+    if (hey == 1){
+        menu.classList.toggle('ouvert');
+        menu_deroulant.classList.toggle('menu_deroulant');
+        // Rétablissez la propriété CSS overflow à auto pour afficher les barres de défilement
+        body.style.overflow = 'auto';
+        hey = 0;
+    }
+    else{
+        menu.classList.toggle('ouvert');
+        menu_deroulant.classList.toggle('menu_deroulant');
+        // Masquez les barres de défilement en définissant la propriété CSS overflow à hidden
+        body.style.overflow = 'hidden';
+        hey = 1;
+    }
+    
+   
 });
 })
+
+
